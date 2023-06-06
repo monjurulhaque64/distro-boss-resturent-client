b20765aec8551e465428e8b0d110ede91a9b0f93
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-06-06 20:41:02 +0600

    category

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Catagory from '../Catagory/Catagory';
+import PropulerMenu from '../PropulerMenu/PropulerMenu';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Catagory></Catagory>
+            <PropulerMenu></PropulerMenu>
         </div>
     );
 };
