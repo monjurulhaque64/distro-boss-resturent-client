03f8857b61de6da4be0e44259fcc3f5b96ccd580
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-06-07 18:54:27 +0600

    menu

--- src/Pages/Home/PropulerMenu/PropulerMenu.jsx
+++ src/Pages/Home/PropulerMenu/PropulerMenu.jsx
@@ -1,17 +1,12 @@
-import React, { useEffect, useState } from 'react';
+
 import SectionTitle from '../../../Components/SectionTitle';
 import MenuItem from '../../Shared/MenuItem/MenuItem';
+import useMenu from '../../../hooks/useMenu';
 
 const PropulerMenu = () => {
-    const [menu, setMenu] = useState([]);
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data.filter(item => item.category === 'popular')
-                setMenu(popularItems)
-            })
-    }, [])
+
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular');
     return (
         <section className='mb-10'>
             <SectionTitle
@@ -20,7 +15,7 @@ const PropulerMenu = () => {
             ></SectionTitle>
             <div className='grid md:grid-cols-2 gap-10'>
                 {
-                    menu.map(item => <MenuItem
+                    popular.map(item => <MenuItem
                         key={item._id}
                         item={item}
                     ></MenuItem>)
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menu from '../../../assets/menu/banner3.jpg'
 
 const Menu = () => {
     return (
@@ -8,7 +10,7 @@ const Menu = () => {
                 <title>Bistro Boss | Menu</title>
                 <link rel="canonical" href="https://www.tacobell.com/" />
             </Helmet>
-            <h2>menu</h2>
+            <Cover img={menu} title={"our menu"}></Cover>
         </div>
     );
 };
